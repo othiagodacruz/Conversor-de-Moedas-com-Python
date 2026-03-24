6171c59da513f3e1ca1b4740f1f2d049520ca6f9
othiagodacruz <thiagofelipedacruz20@gmail.com>
2026-03-23 21:31:48 -0300

    new file:   dados/valores.json 	modified:   interface/interface.py 	new file:   sistema/sistema.py

--- interface/interface.py
+++ interface/interface.py
@@ -1,1 +1,18 @@
 import customtkinter as ctk
+from sistema.sistema import sistema
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+app = ctk.CTk()
+app.geometry("500x200")
+app.title("Conversor de Moedas com Phyton")
+
+valordecima = ctk.CTkEntry(app, placeholder_text="Valor a ser Convertido:")
+
+moedadebaixo = ctk.CTkEntry(app, placeholder_text="Valor Convertido:")
+
+button = ctk.CTkButton(app, text="Converter")
+button.pack(pady=5)
+
+app.mainloop()
---
+++ sistema/sistema.py
@@ -0,0 +1,5 @@
+from interface.interface import interface
+
+def converter():
+    valor = valordecima.get()
+    convertido = moedadebaixo.get()
